a47306e8ce53052de5a8b68c48eb8ec391f60d53
Hersarma <hersarma@gmail.com>
2021-09-28 16:49:29 +0200

    Drying kiln managment

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.guest')
+
+@section('content')
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+        <div class="max-w-md w-full py-12 px-6">
+            <img class="mx-auto h-24 w-auto" src="/img/europalete-text-logo-teal.png" alt="">
+            <p class="mt-6 text-sm text-center text-gray-900">Prijavite se na Vaš nalog</p>
+            <form class="mt-5" method="POST" action="{{ route('login') }}">
+                @csrf
+                <input type="hidden" name="remember" value="true">
+                <div class="rounded-md shadow-sm">
+                    <div>
+                        <input aria-label="Email address" name="email" class="border-gray-300 placeholder-gray-500 appearance-none rounded-none relative block w-full px-3 py-2 border text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Email adresa" value="">
+                        @error('email')
+                        <p class="text-red-500 text-base italic mt-4">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
+                    <div class="-mt-px relative">
+                        <input aria-label="Password" name="password" type="password" class="border-gray-300 placeholder-gray-500 appearance-none rounded-none relative block w-full px-3 py-2 border text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm" placeholder="Lozinka">
+                        @error('password')
+                        <p class="text-red-500 text-base italic mt-4">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                        @if(Route::has('password.request'))
+                            <div class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm">
+                                <a href="{{ route('password.request') }}" class="text-gray-900 underline">Zaboravili ste lozinku?</a>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+
+
+                <div class="mt-5">
+                    <button type="submit" class="relative block w-full py-2 px-3 border border-transparent rounded-md text-white font-semibold bg-gray-800 hover:bg-gray-700 focus:bg-gray-900 focus:outline-none focus:shadow-outline sm:text-sm">
+          <span class="absolute left-0 inset-y pl-3">
+            <svg class="h-5 w-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
+            </svg>
+          </span>
+                        Prijavite se
+                    </button>
+                </div>
+            </form>
+            <div class="mt-6">
+                <div class="relative">
+                    <div class="absolute inset-0 flex items-center">
+                        <div class="w-full border-t border-gray-300"></div>
+                    </div>
+
+                    <div class="relative flex justify-center text-sm">
+                        <span class="px-2 bg-gray-100 text-gray-500">Europalete</span>
+                    </div>
+                </div>
+                @if (Route::has('register'))
+                    <div class="mt-6">
+                        <a href="{{ route('register') }}" class="block w-full text-center py-2 px-3 border border-gray-300 rounded-md text-gray-900 font-medium hover:border-gray-400 focus:outline-none focus:border-gray-400 sm:text-sm">
+                            Registracija
+                        </a>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,105 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+     purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+
+
+    theme: {
+        scale:{
+            '0': '0',
+            '25': '.25',
+            '50': '.5',
+            '75': '.75',
+            '90': '.9',
+            '95': '.95',
+            '100': '1',
+            '101': '1.01',
+            '105': '1.05',
+            '110': '1.1',
+            '125': '1.25',
+            '150': '1.5',
+            '200': '2'
+        },
+        extend: {
+            height: {
+                'screen_nav' : '70vh'
+            },
+            width: {
+                '1/7': '14.2857143%',
+
+                '2/7': '28.5714286%',
+
+                '3/7': '42.8571429%',
+
+                '4/7': '57.1428571%',
+
+                '5/7': '71.4285714%',
+
+                '6/7': '85.7142857%'
+            },
+            spacing: {
+                '128': '32rem',
+                '144': '36rem'
+            },
+            colors: {
+                // Build your palette here
+                transparent: 'transparent',
+                current: 'currentColor',
+                blue_gray: colors.blueGray,
+                
+                gray: colors.coolGray,
+                red: colors.red,
+                blue: colors.lightBlue,
+                yellow: colors.amber,
+                lime: colors.lime,
+                cyan: colors.cyan,
+                fuchsia: colors.fuchsia,
+                teal: colors.teal,
+                turquoise: {
+                    light: '#45c8dc',
+                    medium: '#3fb4c6',
+                    strong: '#258ba1'
+                },
+                
+
+            },
+            animation: {
+
+                'spin-slow': 'spin 3s linear infinite',
+                'spin-fast': 'spin 1s linear infinite'
+            },
+            fontFamily: {
+                sans: ['roboto'],
+                open_sans: ['Open Sans']
+
+
+}
+}
+},
+
+variants: {
+    extend: {
+        zIndex: ['responsive', 'hover'],
+        width: ['responsive', 'hover'],
+        opacity: ['disabled'],
+        borderColor: ['hover,focus, active'],
+        borderCollapse: ['hover', 'focus'],
+        backgroundColor: ['even', 'odd', 'checked'],
+        borderRadius: ['hover', 'focus'],
+        filter: ['hover']
+
+    }
+},
+
+plugins:
+    [
+        require('@tailwindcss/ui')
+
+    ]
+};
