16f990326c32633e4f4d5927a21c20bb19780161
Hersarma <hersarma@gmail.com>
2021-12-03 06:54:59 +0100

    Clients edit modal

--- resources/views/clients/modals/edit.blade.php
+++ resources/views/clients/modals/edit.blade.php
@@ -24,7 +24,7 @@
                 </div>
                 <div class="md:w-2/3">
                   <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg"
-                  id="name" name="name" value="{{ $client->name ?: '/' }}">
+                  id="name" name="name" value="{{ $client->name }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('name') }}
                   </p>
@@ -37,7 +37,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="city" name="city" value="{{ $client->city ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="city" name="city" value="{{ $client->city }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('city') }}
                   </p>
@@ -50,7 +50,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="state" name="state" value="{{ $client->state ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="state" name="state" value="{{ $client->state }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('state') }}
                   </p>
@@ -63,7 +63,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="address_1" name="address_1" value="{{ $client->address_1 ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="address_1" name="address_1" value="{{ $client->address_1 }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('address_1') }}
                   </p>
@@ -76,7 +76,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="address_2" name="address_2" value="{{ $client->address_2 ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="address_2" name="address_2" value="{{ $client->address_2 }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('address_2') }}
                   </p>
@@ -89,7 +89,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="pib" name="pib" value="{{ $client->pib ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="pib" name="pib" value="{{ $client->pib }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('pib') }}
                   </p>
@@ -104,7 +104,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="mb" name="mb" value="{{ $client->mb ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="mb" name="mb" value="{{ $client->mb }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('mb') }}
                   </p>
@@ -117,7 +117,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="contact" name="contact" value="{{ $client->contact ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="contact" name="contact" value="{{ $client->contact }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('contact') }}
                   </p>
@@ -130,7 +130,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="website" name="website" value="{{ $client->website ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="website" name="website" value="{{ $client->website }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('website') }}
                   </p>
@@ -143,7 +143,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="email" name="email" value="{{ $client->email ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="email" name="email" value="{{ $client->email }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('email') }}
                   </p>
@@ -156,7 +156,7 @@
                   </label>
                 </div>
                 <div class="md:w-2/3">
-                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="notes" name="notes" value="{{ $client->notes ?: '/' }}">
+                  <input class="appearance-none bg-gradient-to-r from-blue_gray-800 via-blue_gray-700 to-blue_gray-800 rounded-xl border-l-4 border-gray-400 w-full py-3 px-4 text-gray-200 leading-tight focus:outline-none focus:border-turquoise-light focus:shadow-xl shadow-lg" id="notes" name="notes" value="{{ $client->notes }}">
                   <p class="text-red-500 text-sm italic mt-4">
                     {{ $errors->edit_client->first('notes') }}
                   </p>
