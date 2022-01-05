c029de8846e96eeb1890e70fe024ec369c5cdc64
Hersarma <hersarma@gmail.com>
2022-01-05 13:03:08 +0100

    clients

--- resources/views/clients/search_client.blade.php
+++ resources/views/clients/search_client.blade.php
@@ -5,7 +5,7 @@
     <input type="checkbox" name="deleteChecked[]" value="{{ $client->id }}" class="form-checkbox border-2 border-gray-400 appearance-none checked:bg-green-600 checked:border-transparent px-2 py-2 focus:outline-none">
   </td>
   <td onclick="window.location = '{{ route('clients.show',$client )}}'" class="px-4 py-3 text-left md:text-center text-sm text-gray-200 group-hover:text-teal-600">{{ ucfirst($client->name) }}</td>
-  <td onclick="window.location = '{{ route('clients.show',$client )}}'" class="px-4 py-3 text-left md:text-center text-sm text-gray-200 group-hover:text-teal-600">{{ ucfirst($client->email) }}</td>
+  <td onclick="window.location = '{{ route('clients.show',$client )}}'" class="px-4 py-3 text-left md:text-center text-sm text-gray-200 group-hover:text-teal-600">{{ \Illuminate\Support\Str::limit(ucfirst($client->email) ,20,'...')}}</td>
   <td onclick="window.location = '{{ route('clients.show',$client )}}'" class="hidden md:table-cell px-4 py-3 text-left md:text-center text-sm text-gray-200 group-hover:text-teal-600">{{ ucfirst($client->notes) }}</td>
   <td  class="px-4 py-3 text-right"><a href="{{ route('clients.show',$client )}}"
   class="text-teal-400 hover:text-teal-700"><i class="transition ease-out duration-500 transform hover:scale-110 far fa-eye fa-lg"></i></a></td>
