eb3af663f699777eb2478fe56d9802fb9c915e4c
Hersarma <hersarma@gmail.com>
2022-01-12 20:24:05 +0100

    clients view

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -52,23 +52,6 @@
       </thead>
       <tbody class="searchClient">
         @include('clients.search_client')
-        @if($clients->isEmpty())
-        <tr>
-          <td colspan="6" class="text-center text-gray-200 text-xl p-24">
-            Baza je trenutno prazna
-          </td>
-        </tr>
-        <tr>
-          <td colspan="6" class="overflow-hidden">
-            <div class="flex justify-center mt-5 md:mt-0">
-              <button
-              class="open_modal_create_client transition ease-out duration-500 transform hover:scale-110 py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-teal-400 hover:bg-teal-500 focus:outline-none shadow-xl">
-              Dodaj novog klijenta
-            </button>
-          </div>
-        </td>
-      </tr>
-      @endif
     </tbody>
   </table>
 </form>
--- resources/views/clients/search_client.blade.php
+++ resources/views/clients/search_client.blade.php
@@ -18,3 +18,20 @@
       {{ $clients->links() }}
     </td>
   </tr>
+  @if($clients->isEmpty())
+        <tr>
+          <td colspan="6" class="text-center text-gray-200 text-xl p-24">
+            Nema rezultata
+          </td>
+        </tr>
+        <tr>
+          <td colspan="6" class="overflow-hidden">
+            <div class="flex justify-center mt-5 md:mt-0">
+              <button
+              class="open_modal_create_client transition ease-out duration-500 transform hover:scale-110 py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-teal-400 hover:bg-teal-500 focus:outline-none shadow-xl">
+              Dodaj novog klijenta
+            </button>
+          </div>
+        </td>
+      </tr>
+      @endif
