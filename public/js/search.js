12c1cefcfc1aae3ed3915ad07edc4de0511e0fcd
Hersarma <hersarma@gmail.com>
2022-02-19 20:51:51 +0100

    js

--- public/js/search.js
+++ public/js/search.js
@@ -17,109 +17,7 @@ $(document).ready(function () {
             }
         })
     }
-
-    function fetch_mail_inbox(page, query) {
-        $.ajax({
-            url:"/search_mail_inbox?page=" + page + "&query=" + query,
-            success: function (data) {
-                if (!data){   
-                    $('.searchMailInbox').html('<p class="text-white py-4">Nema rezultata</p>');
-                }
-                else{   
-                    $('.searchMailInbox').html(data);
-                }
-            }
-        })
-    }
-
-    function fetch_incomings(page, query) {
-        $.ajax({
-            url:"/search_incomings?page=" + page + "&query=" + query,
-            success: function (data) {
-                if (!data){   
-                    $('.searchIncoming').html('<p class="text-white py-4">Nema rezultata</p>');
-                }
-                else{   
-                    $('.searchIncoming').html(data);
-                }
-            }
-        })
-    }
-
-     function fetch_outgoings(page, query) {
-        $.ajax({
-            url:"/search_outgoings?page=" + page + "&query=" + query,
-            success: function (data) {
-                if (!data){   
-                    $('#searchOutgoing').html('<p class="text-white py-4">Nema rezultata</p>');
-                }
-                else{   
-                    $('#searchOutgoing').html(data);
-                }
-            }
-        })
-    }
-
-    $(document).on('keyup', '.search_mail_inbox', function () {
-        let query = $(this,'.search_mail_inbox').val();
-        let page = $('#hidden_page').val(1);
-        fetch_mail_inbox(page, query);
-    });
-
-    $(document).on('click', '.paginationsearch_mail_inbox, .paginationmail', function(event){
-      event.preventDefault();
-      let page = $(this).attr('href').split('page=')[1];
-      $('#hidden_page').val(page);
-
-      let query = $(this,'.search_mail_inbox').val();
-      $('.trash').hide();
-      fetch_mail_inbox(page, query);
-    });
-
-    $(document).on('keyup', '.search_incoming', function () {
-        let query = $(this, '.search_incoming').val();
-        let page = $('#hidden_page').val(1);
-        
-        fetch_incomings(page, query);
-    });
-
-    $(document).on('click', '.paginationsearch_incomings, .paginationincoming', function(event){
-      event.preventDefault();
-      let page = $(this).attr('href').split('page=')[1];
-      $('#hidden_page').val(page);
-
-      let query = $('.search_incoming').val();
-      $('.trash').hide();
-      fetch_incomings(page, query);
-    });
-
-    $(document).on('click', '.paginationmobilesearch_incomings, .paginationmobileincoming', function(event){
-      event.preventDefault();
-      let page = $(this).attr('href').split('page=')[1];
-      $('#hidden_page').val(page);
-
-      let query = $('#search_incoming').val();
-      $('.trash').hide();
-      fetch_incomings(page, query);
-    });
-
-    $(document).on('keyup', '#search_outgoing', function () {
-        let query = $('#search_outgoing').val();
-        let page = $('#hidden_page').val(1);
-        fetch_outgoings(page, query);
-    });
-
-    $(document).on('click', '.paginationsearch_outgoings, .paginationoutgoing', function(event){
-      event.preventDefault();
-      let page = $(this).attr('href').split('page=')[1];
-      $('#hidden_page').val(page);
-
-      let query = $('#search_outgoing').val();
-      $('.trash').hide();
-      fetch_outgoings(page, query);
-    });
-    
-    $(document).on('keyup', '.search_clients', function () {
+     $(document).on('keyup', '.search_clients', function () {
         let query = $(this,'.search_clients').val();
         let page = $('#hidden_page').val(1);
         let url_name = $('#url_name').text().split('/')[0];
@@ -146,6 +44,111 @@ $(document).ready(function () {
       $('.trash').hide();
       fetch_clients(query, url_name, page);
     });
+    
+    //Mail Inbox//
+    function fetch_mail_inbox(page, query) {
+        $.ajax({
+            url:"/search_mail_inbox?page=" + page + "&query=" + query,
+            success: function (data) {
+                if (!data){   
+                    $('.searchMailInbox').html('<p class="text-white py-4">Nema rezultata</p>');
+                }
+                else{   
+                    $('.searchMailInbox').html(data);
+                }
+            }
+        })
+    }
+
+    $(document).on('keyup', '.search_mail_inbox', function () {
+        let query = $(this,'.search_mail_inbox').val();
+        let page = $('#hidden_page').val(1);
+        fetch_mail_inbox(page, query);
+    });
+
+    $(document).on('click', '.paginationsearch_mail_inbox, .paginationmail', function(event){
+      event.preventDefault();
+      let page = $(this).attr('href').split('page=')[1];
+      $('#hidden_page').val(page);
+
+      let query = $(this,'.search_mail_inbox').val();
+      $('.trash').hide();
+      fetch_mail_inbox(page, query);
+    });
+
+    //Incomings//
+    function fetch_incomings(page, query) {
+        $.ajax({
+            url:"/search_incomings?page=" + page + "&query=" + query,
+            success: function (data) {
+                if (!data){   
+                    $('.searchIncoming').html('<p class="text-white py-4">Nema rezultata</p>');
+                }
+                else{   
+                    $('.searchIncoming').html(data);
+                }
+            }
+        })
+    }
+    $(document).on('keyup', '.search_incoming', function () {
+        let query = $(this, '.search_incoming').val();
+        let page = $('#hidden_page').val(1);
+        
+        fetch_incomings(page, query);
+    });
+
+    $(document).on('click', '.paginationsearch_incomings, .paginationincoming', function(event){
+      event.preventDefault();
+      let page = $(this).attr('href').split('page=')[1];
+      $('#hidden_page').val(page);
+
+      let query = $('.search_incoming').val();
+      $('.trash').hide();
+      fetch_incomings(page, query);
+    });
+    //Mobile search//
+    $(document).on('click', '.paginationmobilesearch_incomings, .paginationmobileincoming', function(event){
+      event.preventDefault();
+      let page = $(this).attr('href').split('page=')[1];
+      $('#hidden_page').val(page);
+
+      let query = $('#search_incoming').val();
+      $('.trash').hide();
+      fetch_incomings(page, query);
+    });
+
+
+    //Outgoings//
+     function fetch_outgoings(page, query) {
+        $.ajax({
+            url:"/search_outgoings?page=" + page + "&query=" + query,
+            success: function (data) {
+                if (!data){   
+                    $('#searchOutgoing').html('<p class="text-white py-4">Nema rezultata</p>');
+                }
+                else{   
+                    $('#searchOutgoing').html(data);
+                }
+            }
+        })
+    }
+    $(document).on('keyup', '#search_outgoing', function () {
+        let query = $('#search_outgoing').val();
+        let page = $('#hidden_page').val(1);
+        fetch_outgoings(page, query);
+    });
+
+    $(document).on('click', '.paginationsearch_outgoings, .paginationoutgoing', function(event){
+      event.preventDefault();
+      let page = $(this).attr('href').split('page=')[1];
+      $('#hidden_page').val(page);
+
+      let query = $('#search_outgoing').val();
+      $('.trash').hide();
+      fetch_outgoings(page, query);
+    });
+    
+   
 
     $(document).on('keyup', '.search_clients_edit', function () {
         let query = $('.search_clients_edit').val();
