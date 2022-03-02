6acd2ac6dd5009efb9ea10416f211720b88c1c92
Hersarma <hersarma@gmail.com>
2022-03-02 11:46:10 +0100

    Mail delete message

--- resources/views/mail/inbox/search_mail_inbox.blade.php
+++ resources/views/mail/inbox/search_mail_inbox.blade.php
@@ -23,6 +23,7 @@
 <p class="w-1/6 hidden lg:block cursor-pointer get_route_id text-red-600 hover:text-red-700 text-left md:text-center">
   <i class="transition ease-out duration-500 transform hover:scale-110 fas fa-trash fa-lg"></i>
   <span class="hidden">{{route('mail_soft_delete', $mail)}}</span>
+  <em class="hidden">{{ $mail->from }}</em>
 </p>
 </div>
   @else
@@ -48,6 +49,7 @@
 <p class="w-1/6 hidden lg:block cursor-pointer get_route_id text-red-600 hover:text-red-700 text-left md:text-center">
   <i class="transition ease-out duration-500 transform hover:scale-110 fas fa-trash fa-lg"></i>
   <span class="hidden">{{route('mail_soft_delete', $mail)}}</span>
+  <em class="hidden">{{ $mail->from }}</em>
 </p>
 </div>
   @endif
