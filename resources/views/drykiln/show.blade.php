5a2a2e4a836f722df170256049794a0c44485397
Hersarma <hersarma@gmail.com>
2022-03-05 18:32:08 +0100

    drykiln readings

--- resources/views/drykiln/show.blade.php
+++ resources/views/drykiln/show.blade.php
@@ -251,9 +251,38 @@
             <span class="md:hidden lg:hidden"><i class="fa-solid fa-clock text-gray-200"></i></span>
           </p>
         </div>
-        <div class="readings">
-        	@include('drykiln.readings')
+        @foreach($readings as $reading)
+        <div class="group flex justify-between items-center md:px-4">
+          <p class="w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ $reading->moisture_probe_1 ?: '/'}}
+          </p>
+          <p class="w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ $reading->moisture_probe_2 ?: '/'}}
+          </p>
+          <p class="w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ $reading->moisture_probe_3 ?: '/'}}
+          </p>
+          <p class="w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ $reading->moisture_probe_4 ?: '/'}}
+          </p>
+          <p class="w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ $reading->moisture_probe_5 ?: '/'}}
+          </p>
+          <p class="w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ $reading->moisture_probe_6 ?: '/'}}
+          </p>
+          <p class="hidden md:block lg:block w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ $reading->created_at->format('d-m-Y H:i') }}
+          </p>
+          <p class="md:hidden lg:hidden w-1/7 py-3 tracking-wider text-gray-100 text-sm text-center group-hover:text-teal-600">
+            {{ Str::limit($reading->created_at->format('d. F'), 7, $end='') }}
+          </p>
         </div>
+        @endforeach
+        <div class="px-4 py-2">
+      	{{ $readings->links() }}
+      	</div>
+        
   </div>
 	@endif
 @include('drykiln.modals.create_drykiln_config')
