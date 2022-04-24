d1ff0fa0ed1bbe02a5e2ed0867cfd98bac561956
Hersarma <hersarma@gmail.com>
2022-04-24 16:17:48 +0200

    Mail message

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="https://hersarma.in.rs/img/europalete-logo-black_teal.png" class="logo" alt="Laravel Logo">
 @else
 <img src="https://hersarma.in.rs/img/europalete-logo-black_teal.png" class="logo" alt="Europalete logo">
 {{ $slot }}
